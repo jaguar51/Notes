3ed84ced9ce68d3ead765fd457e68b97f411c51e
Yuriy Samsonov <belomore2007@gmail.com>
2015-02-26 19:24:32 +0300

    ViewNoteActivity
    Передача данных для просмотра и редактирования

--- app/src/main/java/me/academeg/notes/ViewNoteActivity.java
+++ app/src/main/java/me/academeg/notes/ViewNoteActivity.java
@@ -1,7 +1,10 @@
 package me.academeg.notes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by Yuriy on 26.02.2015.
@@ -14,5 +17,9 @@ public class ViewNoteActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_note);
 
+
+        Intent intent = getIntent();
+        ((EditText) findViewById(R.id.subjectTxt)).setText(intent.getStringExtra("subject"));
+        ((EditText) findViewById(R.id.noteTxt)).setText(intent.getStringExtra("text"));
     }
 }
