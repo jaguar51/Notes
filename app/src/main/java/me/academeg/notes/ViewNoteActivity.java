a4d4d7688fcb8fbd9b1365059d21c7716b8f0c2c
Yuriy Samsonov <belomore2007@gmail.com>
2015-02-26 19:08:34 +0300

    Activity
    Создание экрана для отображения заметок

---
+++ app/src/main/java/me/academeg/notes/ViewNoteActivity.java
@@ -0,0 +1,18 @@
+package me.academeg.notes;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Yuriy on 26.02.2015.
+ */
+
+public class ViewNoteActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_note);
+
+    }
+}
