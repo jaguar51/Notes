005e897780d566584fa48945d5e7dd4233fdfd2c
Yuriy Samsonov <belomore2007@gmail.com>
2015-03-05 18:08:36 +0300

    Add new function
    1) Функция сохранения связей в файл
    2) Функция чтения связей из файла

--- app/src/main/java/me/academeg/notes/NotesLinksAdapter.java
+++ app/src/main/java/me/academeg/notes/NotesLinksAdapter.java
@@ -4,20 +4,20 @@ import android.content.Context;
 import android.util.Pair;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 
 public class NotesLinksAdapter extends NotesAdapter {
-    private ArrayList<Pair<Long, Long>> links;
+    private ArrayList<Long> links;
     private long ID;
 
-    NotesLinksAdapter(Context context, ArrayList<Note> notes, ArrayList<Pair<Long, Long>> link, long id) {
+    NotesLinksAdapter(Context context, ArrayList<Note> notes, ArrayList<Long> link, long id) {
         super(context, notes);
         links = link;
         ID = id;
-
         //links.add(Pair.create((long) 5, (long) 6));
     }
 
@@ -33,8 +33,8 @@ public class NotesLinksAdapter extends NotesAdapter {
 
         ((TextView)view.findViewById(R.id.subjectTv)).setText(cutText(note.getSubject()));
         ((TextView)view.findViewById(R.id.textTv)).setText(cutText(note.getText()));
-
-
+        //for ()
+        ((CheckBox)view.findViewById(R.id.cbBox)).setChecked(false);
 
         return view;
     }
