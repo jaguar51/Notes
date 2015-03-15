eecb3598de45998064eff9f6219cb784382afe3b
Yuriy Samsonov <belomore2007@gmail.com>
2015-03-15 16:18:33 +0300

    Добавлен функционал, поправлены баги и небольшой рефакторинг
    Добавлена возможность просмотра заметки в AlertDialog при добалении
    связей.
    Баг с переносом текста заметки в элементах списков.

--- app/src/main/java/me/academeg/notes/MainActivity.java
+++ app/src/main/java/me/academeg/notes/MainActivity.java
@@ -21,7 +21,6 @@ import org.xml.sax.InputSource;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
