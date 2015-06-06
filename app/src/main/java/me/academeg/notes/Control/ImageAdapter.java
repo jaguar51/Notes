5ed5c5d9c9360ccd433247e8e4afc90eb31370e0
Yuriy Samsonov <belomore2007@gmail.com>
2015-06-06 17:38:16 +0300

    Step 2. Changed the algorithm calculate the image size

--- app/src/main/java/me/academeg/notes/Control/ImageAdapter.java
+++ app/src/main/java/me/academeg/notes/Control/ImageAdapter.java
@@ -90,7 +90,6 @@ public class ImageAdapter extends BaseAdapter {
     public Bitmap decodeSampledBitmapFromUri(String path, int reqWidth,
                                              int reqHeight) {
 
-        Bitmap bm = null;
         final BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
         BitmapFactory.decodeFile(path, options);
@@ -98,15 +97,15 @@ public class ImageAdapter extends BaseAdapter {
         // Calculate inSampleSize
         options.inSampleSize = calculateInSampleSize(options, reqWidth,
                 reqHeight);
+
         // Decode bitmap with inSampleSize set
         options.inJustDecodeBounds = false;
         options.inPreferredConfig = Bitmap.Config.RGB_565;
-        bm = BitmapFactory.decodeFile(path, options);
 
-        return bm;
+        return BitmapFactory.decodeFile(path, options);
     }
 
-    public int calculateInSampleSize(
+    public static int calculateInSampleSize(
             BitmapFactory.Options options, int reqWidth, int reqHeight) {
         // Raw height and width of image
         final int height = options.outHeight;
@@ -114,11 +113,15 @@ public class ImageAdapter extends BaseAdapter {
         int inSampleSize = 1;
 
         if (height > reqHeight || width > reqWidth) {
-            if (width > height) {
-                inSampleSize = Math.round((float) height
-                        / (float) reqHeight);
-            } else {
-                inSampleSize = Math.round((float) width / (float) reqWidth);
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
             }
         }
 
